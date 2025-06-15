1d6941a59036138d759b31f1372ab73361f6f44d
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-06-15 22:40:44 +0100

    implement interview generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const Page = () => {
+  return (
+   <>
+   
+   <h3>INTERVIEW GENERATION</h3>
+   <Agent/>
+   </>
+  )
+}
+
+export default Page
