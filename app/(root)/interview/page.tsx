509ae8138394fc872626a45cb191106ada195baf
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-06-18 23:11:13 +0100

    implement interview generation

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,13 @@
-import Agent from '@/components/Agent'
-import React from 'react'
+import Agent from "@/components/Agent";
+import React from "react";
 
 const Page = () => {
   return (
-   <>
-   
-   <h3>INTERVIEW GENERATION</h3>
-   <Agent/>
-   </>
-  )
-}
+    <>
+      <h3>INTERVIEW GENERATION</h3>
+      <Agent userName="You" userId="user1" type="generate" />
+    </>
+  );
+};
 
-export default Page
+export default Page;
